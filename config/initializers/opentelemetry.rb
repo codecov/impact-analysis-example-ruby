a127694ea5116fe7bc529cbe8ea7e819de6603b4
Tom Hu <tom@codecov.io>
2022-06-16 22:00:53 -0400

    feat: add opentelem

---
+++ config/initializers/opentelemetry.rb
@@ -0,0 +1,28 @@
+require "coverage"
+
+require 'opentelemetry/sdk'
+require 'opentelemetry/exporter/otlp'
+require 'opentelemetry/instrumentation/all'
+require 'codecov_opentelem'
+
+OpenTelemetry::SDK.configure do |c|
+    c.service_name = 'Codecov'
+    c.use_all() # enables all instrumentation!
+
+    current_env = ENV['CODECOV_OPENTELEMETRY_ENV'] || 'production'
+    current_version = ENV['CURRENT_VERSION'] || '0.0.1'
+    export_rate = 100
+    untracked_export_rate = 0
+
+    generator, exporter = get_codecov_opentelemetry_instances(
+        repository_token: ENV['CODECOV_OPENTELEMETRY_TOKEN'],
+        sample_rate: export_rate,
+        untracked_export_rate: untracked_export_rate,
+        code: "#{current_version}:#{current_env}",
+        version_identifier: current_version,
+        environment: current_env,
+    )
+
+    c.add_span_processor(generator)
+    c.add_span_processor(OpenTelemetry::SDK::Trace::Export::BatchSpanProcessor.new(exporter))
+end
