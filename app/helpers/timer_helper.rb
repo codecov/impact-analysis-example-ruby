968b924c2f6392a234cb3fcf1fabc28fec882a8c
Tom Hu <tom@codecov.io>
2022-07-26 22:55:24 -0700

    more things

--- app/helpers/timer_helper.rb
+++ app/helpers/timer_helper.rb
@@ -1,10 +1,13 @@
 module TimerHelper
   def format_time(time)
-    run_rand() if rand_run
+    run_rand_10 if rand_run(0.1)
+    run_rand_20 if rand_run(0.2)
+    run_rand_50 if rand_run(0.5)
+    run_rand_90 if rand_run(0.9)
     time.strftime('%Y-%m-%d--%H:%M:%S')
   end
 
-  def rand_run(value=0.2)
+  def rand_run(value)
     rand() <= value
   end
 end
