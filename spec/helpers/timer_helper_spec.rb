1edc3b1aaaf3517b934dc7eed8a11f4eed2988d7
Tom Hu <tom@codecov.io>
2022-08-31 22:00:20 -0400

    fix: forgot a do

--- spec/helpers/timer_helper_spec.rb
+++ spec/helpers/timer_helper_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 describe TimerHelper do
   describe "time format" do
     it "returns correctly formatted time" do
-      100.times
+      100.times do
         current_time = Time.now()
         expect(helper.format_time(current_time)).to eql(current_time.strftime('%Y-%m-%d::%H:%M:%S'))
       end
