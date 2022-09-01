3c7a464302892f788fdcffc5152171d3c6b247d5
Tom Hu <tom@codecov.io>
2022-08-31 22:01:31 -0400

    fix: revert extra colon

--- spec/helpers/timer_helper_spec.rb
+++ spec/helpers/timer_helper_spec.rb
@@ -5,7 +5,7 @@ describe TimerHelper do
     it "returns correctly formatted time" do
       100.times do
         current_time = Time.now()
-        expect(helper.format_time(current_time)).to eql(current_time.strftime('%Y-%m-%d::%H:%M:%S'))
+        expect(helper.format_time(current_time)).to eql(current_time.strftime('%Y-%m-%d:%H:%M:%S'))
       end
     end
   end
