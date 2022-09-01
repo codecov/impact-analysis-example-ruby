d94793bf2f7e6475fdde414acf042b7c20c96ef0
Tom Hu <tom@codecov.io>
2022-08-31 21:55:53 -0400

    fix: run tests 100 times for randomness

--- spec/helpers/timer_helper_spec.rb
+++ spec/helpers/timer_helper_spec.rb
@@ -3,8 +3,10 @@ require "rails_helper"
 describe TimerHelper do
   describe "time format" do
     it "returns correctly formatted time" do
-      current_time = Time.now()
-      expect(helper.format_time(current_time)).to eql(current_time.strftime('%Y-%m-%d:%H:%M:%S'))
+      100.times
+        current_time = Time.now()
+        expect(helper.format_time(current_time)).to eql(current_time.strftime('%Y-%m-%d::%H:%M:%S'))
+      end
     end
   end
 end
